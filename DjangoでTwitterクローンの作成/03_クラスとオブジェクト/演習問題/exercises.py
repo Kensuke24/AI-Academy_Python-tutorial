a38ca335240ccc99b53fc3d35a44fdb25d84b126
Kensuke24 <hibis.tori@gmail.com>
2019-03-03 18:45:15 +0900

    演習＃１を入力しました。

--- DjangoでTwitterクローンの作成/03_クラスとオブジェクト/演習問題/exercises.py
+++ DjangoでTwitterクローンの作成/03_クラスとオブジェクト/演習問題/exercises.py
@@ -1,4 +1,19 @@
 # このファイルは演習問題の解答です。
 
-#1
-class UserScore:  #UserScoreクラスの作成
+# 1  expected 2 blank lines, found 1 = PEP8チェック。コメントの下のは2行の空白が必要
+
+
+class UserScore:    # UserScoreクラスの作成
+    def __init__(self, score):  # メソッドの作成。メソッドとはクラス内で定義されている関数。
+        # メソッドの1つ目の引数にはselfを入れる。'self'というのはクラスのインスタンス自身を指す。
+        self.score = score  # self自体が'SampleClass'のインスタンスをさしますので、nameと言うインスタンス変数を用意して引数nameを代入します。
+
+
+u = UserScore({'english': 100, 'math': 49})
+u1 = UserScore({'english': 80, 'math': 83})  # PEP8チェック。'：'の後にはスペースが必要。
+
+print(u.score)
+print(u1.score)
+
+
+
