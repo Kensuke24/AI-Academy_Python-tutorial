64efc357079ba557c21779b7042e509f9671e0da
Kensuke24 <hibis.tori@gmail.com>
2019-03-03 22:54:51 +0900

    演習問題の解答

--- DjangoでTwitterクローンの作成/03_クラスとオブジェクト/演習問題/exercises.py
+++ DjangoでTwitterクローンの作成/03_クラスとオブジェクト/演習問題/exercises.py
@@ -2,10 +2,12 @@
 
 # 1  expected 2 blank lines, found 1 = PEP8チェック。コメントの下には空白が必要
 
+
 class UserScore:    # UserScoreクラスの作成
     def __init__(self, score):  # メソッドの作成。メソッドとはクラス内で定義されている関数。
-        # メソッドの1つ目の引数にはselfを入れる。'self'というのはクラスのインスタンス自身を指す。
-        self.score = score  # self自体が'SampleClass'のインスタンスをさしますので、nameと言うインスタンス変数を用意して引数nameを代入します。
+        # メソッドの1番目の引数はselfという名前にする。'self'というのはクラスのインスタンス自身を指す。
+        self.score = score  # self自体が'SampleClass'のインスタンスを指すので、nameと言うインスタンス変数を用意して引数nameを代入します。
+
 
 u = UserScore({'english': 100, 'math': 49})
 u1 = UserScore({'english': 80, 'math': 83})  # PEP8チェック。'：'の後にはスペースが必要。
@@ -13,14 +15,28 @@ u1 = UserScore({'english': 80, 'math': 83})  # PEP8チェック。'：'の後に
 print(u.score)
 print(u1.score)
 
-#2
+# 2
 
-class User:
-    def __init__(selfself, user_id, usernema, email):
+
+class User:  # Userクラスの作成。
+    def __init__(self, user_id, username, email):
         self.user_id = user_id
-        seif.username = username
+        self.username = username
         self.email = email
-    #3
+    # 3
 
     def get_userinfo(self):
+        print(self.username)
+        print(self.email)
+
+
+user1 = User(1, 'test', 'test@gmail.com')
+user2 = User(2, 'test2', 'test2@gmail.com')
+
+# 3
+
+user1.get_userinfo()
+user2.get_userinfo()
+
+
 
